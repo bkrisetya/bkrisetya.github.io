dc95c3a25fddab62ee3220f7a8af872cd88c4c37
bkrisetya <49872361+bkrisetya@users.noreply.github.com>
2025-12-04 01:29:13 +0000

    Deploying to gh-pages from @ bkrisetya/bkrisetya.github.io@ca8c3b994341f0d14ce4e6129e44b6ecd4fbaf35 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,25 +497,18 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62@%6B%72%69%73%65%74%79%61.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/", "_blank");
         },
       },{
         id: 'social-rss',
