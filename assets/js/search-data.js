2a9ce06efd9feb05eecaaad8782fe61ac023274f
bkrisetya <49872361+bkrisetya@users.noreply.github.com>
2025-12-05 17:55:58 +0000

    Deploying to gh-pages from @ bkrisetya/bkrisetya.github.io@58de70b0fa57fbfc7802396ae50dd87e321d026d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,28 +508,7 @@ ninja.data = [{
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/{"logo"=>"https://www.alberteinstein.com/wp-content/uploads/2024/03/cropped-favicon-192x192.png", "title"=>"Custom Social", "url"=>"https://www.alberteinstein.com/"}", "_blank");
         },
       },{
       id: 'light-theme',
